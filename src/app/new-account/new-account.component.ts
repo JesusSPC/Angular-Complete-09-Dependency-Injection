e7c494755115659e824378746fb7d64592030781
JesusSP <jesus.saints@gmail.com>
2020-05-18 11:21:43 +0200

    Course completed

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { LoggingService } from '../services/login.service';
+import { AccountsService } from '../services/account.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent {
+
+  constructor(
+    // private loggingService: LoggingService,
+    private accountService: AccountsService
+  ) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+  }
+}
